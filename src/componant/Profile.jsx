3772d2e519c31daeb3e26a1a5100df012d28497d
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-28 18:03:00 +0600

    alsiamDetails

--- src/componant/Profile.jsx
+++ src/componant/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import cover from '../assets/cover.jpg'
 import Logo from '../assets/img.jpg'
+import Card from '../assets/Card.png'
 const Profile = () => {
     return (
         <section className='py-[10px] bg-[#1f1f1f]'>
@@ -11,10 +12,11 @@ const Profile = () => {
                     </div>
                     <div className="ProImg w-[160px] absolute lg:left-[100px] bottom-[-40%] h-[160px] flex justify-center items-center rounded-[50%] bg-[#000000]">
                         <img src={Logo} className='w-[150px] h-[150px] rounded-[50%]' alt="" />
+                    <div className="active w-[14px] absolute bottom-[18px] right-[18px] h-[14px] bg-[green] rounded-[50%]"></div>
                     </div>
                 </div>
                 <div className="Download flex justify-end mx-auto my-[20px] lg:w-[90%]">
-                    <a download={Logo} href='Logo'>
+                    <a download={Card} href={Card}>
                         <h2 className='w-[100px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[15px] rounded-[10px] bg-[red]'>Save Details</h2>
                     </a>
                 </div>
