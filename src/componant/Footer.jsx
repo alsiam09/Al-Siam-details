6bcab64285d481be6828d901cf90ae0d6b5fe86c
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-29 23:53:28 +0600

    alsiamDetails

---
+++ src/componant/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='bg-[#262626] py-[30px]'>
+        <div className="container mx-auto px-[10px] text-center lg:px-[0]">
+            <a className='text-[#ffabab] text-[#25px]' href="mailto : gmalsiam4200@gmail.com">gmalsiam4200@gmail.com</a>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Profile from '../componant/Profile'
 import ManDetailes from '../componant/ManDetailes'
+import Footer from '../componant/Footer'
 const Home = () => {
   return (
     <>
     <Profile/>
     <ManDetailes/>
+    <Footer/>
     </>
   )
 }
