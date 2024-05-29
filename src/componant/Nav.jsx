f670f9910066806d211af8d2a6de1c201165ac0f
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-05-29 14:24:45 +0600

    alsiamDetails

--- src/componant/Nav.jsx
+++ src/componant/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <section className='bg-[#1f1f1f] border-b-[3px] border-b-[red]'>
+    <section className=' fixed w-[100%] top-0 left-0 z-[999] bg-[#1f1f1f] border-b-[3px] border-b-[red]'>
         <div className="container px-[10px] lg:px-0 mx-auto h-[80px]">
             <div className="NavItem flex items-center h-[100%] w-[100%]">
                 <h1 className=' font-alsiam mx-auto md:mx-0 text-[#fff] text-[30px]'>Al-Siam</h1>
--- src/componant/Profile.jsx
+++ src/componant/Profile.jsx
@@ -6,7 +6,7 @@ import { MdCall } from "react-icons/md";
 import { FaWhatsapp } from "react-icons/fa6";
 const Profile = () => {
     return (
-        <section className='py-[10px] bg-[#1f1f1f]'>
+        <section className='py-[10px] pt-[90px] bg-[#1f1f1f]'>
             <div className="container px-[10px] lg:px-[0px] mx-auto">
                 <div className=" w-[100%] relative profileImg">
                     <div className=" flex justify-center coverImg">
@@ -18,8 +18,8 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className="Download flex gap-1 justify-end mx-auto my-[20px] lg:w-[90%]">
-                    <a href="tel:01813904257" className='w-[40px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[20px] rounded-[10px] border-[2px] border-[red]' ><MdCall/></a>
-                    <a href="https://wa.me/message/3RGTSTCX3MO2G1" className='w-[40px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[20px] rounded-[10px] border-[2px] border-[red]' ><FaWhatsapp/></a>
+                    <a href="tel:01813904257" className=' hover:bg-[#fff] hover:text-[red] w-[40px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[20px] rounded-[10px] border-[2px] border-[red]' ><MdCall/></a>
+                    <a href="https://wa.me/message/3RGTSTCX3MO2G1" className=' hover:bg-[#fff] hover:text-[red] w-[40px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[20px] rounded-[10px] border-[2px] border-[red]' ><FaWhatsapp/></a>
                     <a download='alsiamCard.jpg' href={Card}>
                         <h2 className='w-[100px] h-[40px] flex justify-center items-center font-alsiam text-[#fff] text-[15px] rounded-[10px] bg-[red]'>Save Details</h2>
                     </a>
